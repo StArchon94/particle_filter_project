5cb7b52d3714f850f4fa1930bdf0b42c6b8fc0cb
Shengjie Lin <StArchon@outlook.com>
2022-04-22 17:07:26 -0500

    implemented all modules

--- scripts/particle_filter.py
+++ scripts/particle_filter.py
@@ -23,22 +23,11 @@ from likelihood_field import LikelihoodField
 def get_yaw_from_pose(p):
     """ A helper function that takes in a Pose object (geometry_msgs) and returns yaw"""
 
-    yaw = (euler_from_quaternion([
+    return euler_from_quaternion([
         p.orientation.x,
         p.orientation.y,
         p.orientation.z,
-        p.orientation.w])
-        [2])
-
-    return yaw
-
-
-def draw_random_sample():
-    """ Draws a random sample of n elements from a given list of choices and their specified probabilities.
-    We recommend that you fill in this function using random_sample.
-    """
-    # TODO
-    return
+        p.orientation.w])[2]
 
 
 def compute_prob_zero_centered_gaussian(dist, sd):
@@ -51,7 +40,7 @@ def compute_prob_zero_centered_gaussian(dist, sd):
 
 class Particle:
 
-    def __init__(self, pose, w):
+    def __init__(self, pose, w=1):
 
         # particle pose (Pose object from geometry_msgs)
         self.pose = pose
@@ -60,11 +49,25 @@ class Particle:
         self.w = w
 
 
+class Map:
+    def __init__(self, map) -> None:
+        h = map.info.height
+        w = map.info.width
+        self.map = np.reshape(map.data, (h, w))
+        self.res = map.info.resolution
+        o = map.info.origin.position
+        self.x0 = o.x
+        self.y0 = o.y
+
+    def query(self, x, y):
+        r = round((y - self.y0) / self.res)
+        c = round((x - self.x0) / self.res)
+        return self.map[r, c]
+
+
 class ParticleFilter:
-
     def __init__(self):
-
-        # once everything is setup initialized will be set to true
+        # once everything is setup, initialized will be set to true
         self.initialized = False
 
         # initialize this particle filter node
@@ -79,9 +82,12 @@ class ParticleFilter:
         # inialize our map
         self.map = OccupancyGrid()
         self.likelihood_field = LikelihoodField()
+        (self.l, self.r), (self.b, self.t) = self.likelihood_field.get_obstacle_bounding_box()
+        self.map = Map(self.likelihood_field.map)
 
         # the number of particles used in the particle filter
-        self.num_particles = 10000
+        # self.num_particles = 10000
+        self.num_particles = 30
 
         # initialize the particle cloud array
         self.particle_cloud = []
@@ -91,7 +97,7 @@ class ParticleFilter:
 
         # set threshold values for linear and angular movement before we preform an update
         self.lin_mvmt_threshold = 0.2
-        self.ang_mvmt_threshold = (np.pi / 6)
+        self.ang_mvmt_threshold = np.pi / 6
 
         self.odom_pose_last_motion_update = None
 
@@ -104,7 +110,7 @@ class ParticleFilter:
         self.robot_estimate_pub = rospy.Publisher("estimated_robot_pose", PoseStamped, queue_size=10)
 
         # subscribe to the map server
-        rospy.Subscriber(self.map_topic, OccupancyGrid, self.get_map)
+        # rospy.Subscriber(self.map_topic, OccupancyGrid, self.get_map)
 
         # subscribe to the lidar scan from the robot
         rospy.Subscriber(self.scan_topic, LaserScan, self.robot_scan_received)
@@ -118,22 +124,38 @@ class ParticleFilter:
 
         self.z_hit = 0.8
         self.z_random = 0.1
-        self.z_max = 0.1
-        self.sigma = 0.3
+        self.sigma_dist = 0.3
+
+        self.sigma_r = 0.02
+        self.sigma_t = 0.01
 
         self.initialized = True
 
-    def get_map(self, data):
-
-        self.map = data
+    # def get_map(self, data):
+    #     self.map = data
+    #     p = data.info.origin.position
+    #     self.l = p.x
+    #     self.b = p.y
+    #     self.t =
 
     def initialize_particle_cloud(self):
-
-        # TODO
+        i = 0
+        while i < self.num_particles:
+            x = np.random.uniform(self.l, self.r)
+            y = np.random.uniform(self.b, self.t)
+            # check if the particle's position is free space in the map
+            if not self.map.query(x, y):
+                ang = np.random.uniform(-np.pi, np.pi)
+                p = Particle(Pose(Point(x, y, 0), Quaternion(*quaternion_from_euler(0, 0, ang))))
+                self.particle_cloud.append(p)
+                i += 1
 
         self.normalize_particles()
+        rospy.sleep(1)
 
         self.publish_particle_cloud()
+        self.update_estimated_robot_pose()
+        self.publish_estimated_robot_pose()
 
     def normalize_particles(self):
         # make all the particle weights sum to 1.0
@@ -145,7 +167,7 @@ class ParticleFilter:
 
         particle_cloud_pose_array = PoseArray()
         particle_cloud_pose_array.header = Header(stamp=rospy.Time.now(), frame_id=self.map_topic)
-        particle_cloud_pose_array.poses
+        # particle_cloud_pose_array.poses
 
         for part in self.particle_cloud:
             particle_cloud_pose_array.poses.append(part.pose)
@@ -177,13 +199,12 @@ class ParticleFilter:
 
         # wait for a little bit for the transform to become avaliable (in case the scan arrives
         # a little bit before the odom to base_footprint transform was updated)
-        self.tf_listener.waitForTransform(self.base_frame, self.odom_frame, data.header.stamp, rospy.Duration(0.5))
-        if not(self.tf_listener.canTransform(self.base_frame, data.header.frame_id, data.header.stamp)):
+        if not self.tf_listener.waitForTransform(self.base_frame, self.odom_frame, data.header.stamp, rospy.Duration(0.5)):
             return
 
         # calculate the pose of the laser distance sensor
         p = PoseStamped(
-            header=Header(stamp=rospy.Time(0),
+            header=Header(stamp=rospy.Time.now(),
                           frame_id=data.header.frame_id))
 
         self.laser_pose = self.tf_listener.transformPose(self.base_frame, p)
@@ -236,14 +257,19 @@ class ParticleFilter:
 
     def update_estimated_robot_pose(self):
         # based on the particles within the particle cloud, update the robot pose estimate
-
-        # TODO
-        pass
+        x_sum = 0
+        y_sum = 0
+        ang_sum = 0
+        for p in self.particle_cloud:
+            x_sum += p.pose.position.x
+            y_sum += p.pose.position.y
+            ang_sum += get_yaw_from_pose(p.pose)
+        self.robot_estimate = Pose(Point(x_sum / self.num_particles, y_sum / self.num_particles, 0), Quaternion(*quaternion_from_euler(0, 0, ang_sum / self.num_particles)))
 
     def update_particle_weights_with_measurement_model(self, data):
         for particle in self.particle_cloud:
             q = 1
-            ang = euler_from_quaternion([particle.pose.orientation.x, particle.pose.orientation.y, particle.pose.orientation.z, particle.pose.orientation.w])[2]
+            ang = get_yaw_from_pose(particle.pose)
             for i in range(360):
                 z = data.ranges[i]
                 if not data.range_min <= z <= data.range_max:
@@ -251,20 +277,36 @@ class ParticleFilter:
                 x = particle.pose.position.x + z * np.cos(i / 180 * np.pi + ang)
                 y = particle.pose.position.y + z * np.sin(i / 180 * np.pi + ang)
                 dist = self.likelihood_field.get_closest_obstacle_distance(x, y)
-                q *= self.z_hit * compute_prob_zero_centered_gaussian(dist, self.sigma) + self.z_random / self.z_max
+                q *= self.z_hit * compute_prob_zero_centered_gaussian(dist, self.sigma_dist) + self.z_random / data.range_max
             particle.w = q
 
     def update_particles_with_motion_model(self):
-
         # based on the how the robot has moved (calculated from its odometry), we'll  move
         # all of the particles correspondingly
-        pass
-
-        # TODO
+        p1 = self.odom_pose_last_motion_update.pose
+        x1 = p1.position.x
+        y1 = p1.position.y
+        theta1 = get_yaw_from_pose(p1)
+        p2 = self.odom_pose.pose
+        x2 = p2.position.x
+        y2 = p2.position.y
+        theta2 = get_yaw_from_pose(p2)
+        r1 = np.arctan2(y2 - y1, x2 - x1) - theta1
+        t = np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+        r2 = theta2 - theta1 - r1
+        for particle in self.particle_cloud:
+            r1_ = r1 + np.random.standard_normal() * self.sigma_r
+            t_ = t + np.random.standard_normal() * self.sigma_t
+            r2_ = r2 + np.random.standard_normal() * self.sigma_r
+            p = particle.pose
+            x = p.position.x
+            y = p.position.y
+            theta = get_yaw_from_pose(p)
+            p.position.x += t_ * np.cos(theta + r1_)
+            p.position.y += t_ * np.sin(theta + r1_)
+            p.orientation = Quaternion(*quaternion_from_euler(0, 0, theta + r1_ + r2_))
 
 
 if __name__ == "__main__":
-
-    pf = ParticleFilter()
-
+    ParticleFilter()
     rospy.spin()
